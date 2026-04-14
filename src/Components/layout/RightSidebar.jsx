a75d677a3f69848aa12fce9472d6408b96bbc451
Myst-12 <kushal11.mittal@gmail.com>
2026-04-14 23:14:40 +0530

    Right Sidebar Updated

--- src/Components/layout/RightSidebar.jsx
+++ src/Components/layout/RightSidebar.jsx
@@ -3,17 +3,46 @@ import React from 'react'
 const RightSidebar = () => {
 
   const suggestions = [
-    {name:"user 1", info:"Suggested for you"},
-    {name:"user 2", info:"Suggested for you"},
-    {name:"user 3", info:"New to Instagram"},
-    {name:"user 4", info:"New to Instagram"}
+    { name: "Daksh", info: "Follows you" },
+    { name: "Sourav", info: "New to Instagram" },
+    { name: "Tawde", info: "Suggested for you" },
+    { name: "Desai", info: "Popular" }
   ]
 
-
-
   return (
-    <div className='RightSidebar'>
-      wow
+    <div className="RightSidebar">
+
+      <div className="userProfile">
+        <div className="left">
+          <div className="avatar"></div>
+          <div>
+            <p className="username">Kushal</p>
+            <p className="subText">...</p>
+          </div>
+        </div>
+        <p className="Switch">Switch</p>
+      </div>
+
+      <div className="SuggestionsHeader">
+        <p>Suggested for you</p>
+        <p className="seeAll">See All</p>
+      </div>
+
+      <div className="SuggestionsBox">
+        {suggestions.map((user, index) => (
+          <div key={index} className="suggestionItem">
+            <div className="left">
+              <div className="avatar small"></div>
+              <div>
+                <p className="Username">{user.name}</p>
+                <p className="Info">{user.info}</p>
+              </div>
+            </div>
+            <p className="Follow">Follow</p>
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 }
