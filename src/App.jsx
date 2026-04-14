3607b8cf87856c98a20804be7b1afad8e6728f1f
Myst-12 <kushal11.mittal@gmail.com>
2026-04-14 22:02:37 +0530

    made the basic structure and preparing the right side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Sidebar from "./Components/layout/Sidebar"
+import RightSidebar from "./Components/layout/RightSidebar"
+import FeedLayout from "./Components/layout/FeedLayout"
 
 
 function App() {
   return(
-    <div>
+    <div className="Home">
       <Sidebar />
+      <FeedLayout />
+      <RightSidebar />
     </div>
   )
 }
--- src/Components/layout/RightSidebar.jsx
+++ src/Components/layout/RightSidebar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const RightSidebar = () => {
+
+  const suggestions = [
+    {name:"user 1", info:"Suggested for you"},
+    {name:"user 2", info:"Suggested for you"},
+    {name:"user 3", info:"New to Instagram"},
+    {name:"user 4", info:"New to Instagram"}
+  ]
+
+
+
   return (
-    <div>RightSidebar</div>
+    <div className='RightSidebar'>
+      wow
+    </div>
   )
 }
 
